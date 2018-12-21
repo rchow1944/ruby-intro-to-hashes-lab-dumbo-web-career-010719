839a46c72c644ad253704ce0da403e644ac048a5
Raymond Chow <raychow1944@yahoo.com>
2018-12-21 23:57:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
   	    names: {
   	      reading_railroad: {},
   	      pennsylvania_railroad: {},
-  	      reading_railroad: {},
+  	      b_and_o_railroad: {},
   	      reading_railroad: {}
   	    },
   	    rent_in_dollars: {
